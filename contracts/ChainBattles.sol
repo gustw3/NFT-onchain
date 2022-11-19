e048f0d2218f8e4a91de33981b065416d8b950a8
gustw3 <contact@augustinbrun.com>
2022-11-19 11:22:59 +0100

    onchain nft

---
+++ contracts/ChainBattles.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract ChainBattles is ERC721URIStorage {
+	using Strings for uint256;
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIds;
+
+		struct Warrior {
+		mapping(uint256 => uint256) tokenIdtoLevels;
+		mapping(uint256 => uint256) tokenIdtoSpeeds;
+		mapping(uint256 => uint256) tokenIdtoStrengths;
+		mapping(uint256 => uint256) tokenIdtoLifes;
+	}
+
+	Warrior warrior;
+
+
+
+	constructor() ERC721("Chain Battles", "CBTLS") {
+
+	}
+
+	function generateCharacter(uint256 tokenId) public returns(string memory){
+
+			bytes memory svg = abi.encodePacked(
+					'<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+					'<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
+					'<rect width="100%" height="100%" fill="black" />',
+					'<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
+					'<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+					'<text x="50%" y="55%" class="base" dominant-baseline="middle" text-anchor="middle">', "Speeds: ",getSpeeds(tokenId),'</text>',
+					'<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">', "Strengths: ",getStrengths(tokenId),'</text>',
+					'<text x="50%" y="65%" class="base" dominant-baseline="middle" text-anchor="middle">', "Lifes: ",getLifes(tokenId),'</text>',
+					'</svg>'
+			);
+			return string(
+					abi.encodePacked(
+							"data:image/svg+xml;base64,",
+							Base64.encode(svg)
+					)
+			);
+	}
+
+	function getLevels(uint256 tokenId) public view returns (string memory) {
+			uint256 levels = warrior.tokenIdtoLevels[tokenId];
+			return levels.toString();
+	}
+	function getSpeeds(uint256 tokenId) public view returns (string memory) {
+			uint256 speeds = warrior.tokenIdtoSpeeds[tokenId];
+			return speeds.toString();
+	}
+
+	function getStrengths(uint256 tokenId) public view returns (string memory) {
+			uint256 strengths = warrior.tokenIdtoStrengths[tokenId];
+			return strengths.toString();
+	}
+
+	function getLifes(uint256 tokenId) public view returns (string memory) {
+			uint256 lifes = warrior.tokenIdtoLifes[tokenId];
+			return lifes.toString();
+	}
+
+	function getTokenURI(uint256 tokenId) public returns (string memory){
+			bytes memory dataURI = abi.encodePacked(
+					'{',
+							'"name": "Chain Battles #', tokenId.toString(), '",',
+							'"description": "Battles on chain",',
+							'"image": "', generateCharacter(tokenId), '"',
+					'}'
+			);
+			return string(
+					abi.encodePacked(
+							"data:application/json;base64,",
+							Base64.encode(dataURI)
+					)
+			);
+	}
+
+	function mint() public {
+		_tokenIds.increment();
+		uint256 newItemId = _tokenIds.current();
+		_safeMint(msg.sender, newItemId);
+		warrior.tokenIdtoLevels[newItemId] = 0;
+		_setTokenURI(newItemId, getTokenURI(newItemId));
+	}
+
+	function train(uint256 tokenId) public {
+		require(_exists(tokenId), "Please use an existing Token");
+		require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
+		uint256 currentLevel = warrior.tokenIdtoLevels[tokenId];
+		uint currentSpeed = warrior.tokenIdtoSpeeds[tokenId];
+		uint currentStrength = warrior.tokenIdtoStrengths[tokenId];
+		uint currentLife = warrior.tokenIdtoLifes[tokenId];
+
+		warrior.tokenIdtoLevels[tokenId] = currentLevel + 1;
+		warrior.tokenIdtoSpeeds[tokenId] = currentSpeed + 1;
+		warrior.tokenIdtoStrengths[tokenId] = currentStrength + 1;
+		warrior.tokenIdtoLifes[tokenId] = currentLife + 1;
+
+		_setTokenURI(tokenId, getTokenURI(tokenId));
+
+	}
+}
